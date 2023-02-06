38e32509eb28250e8baed9eae973dbd5135e26cb
Aswitha-Ajin <1112aswitha@gmail.com>
2023-02-06 06:20:48 +0530

    commit

---
+++ Route/category.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const Category = require("../Module/Category");
+
+router.post("/", async (req, res) => {
+    const newcat = new Category(req.body);
+    try {
+        const store = await newcat.save()
+        res.status(200).json(store)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+router.get("/", async (req, res) => {
+    
+    try {
+        const stored = await Category.find()
+        res.status(200).json(stored)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,41 @@
+const express=require("express") ;
+const app= express();
+const dotenv=require("dotenv");
+const mongoose=require("mongoose");
+const Authentication=require("./Route/Auth");
+const users=require("./Route/user");
+const Post=require("./Route/post");
+const Categories=require("./Route/category");
+const multer=require("multer");
+dotenv.config();
+
+
+app.use(express.json());
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.MONGODB_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+     //useCreateIndex: true,
+ 
+}).then(console.log("Connected")).catch((err)=>console.log(err));
+
+const storage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,"image");
+    },
+    filename:(req,file,cb)=>{
+        cb(null,req.body.name);
+    },
+})
+const upload=multer({storage:storage});
+app.post("/api/upload",upload.single("file"),(req,res)=>{
+res.status(200).json("File  uploaded")
+});
+app.use("/api/auth",Authentication);
+app.use("/api/user",users);
+app.use("/api/post",Post);
+app.use("/api/category",Categories)
+
+app.listen("5000",()=>{
+    console.log("running")
+});
